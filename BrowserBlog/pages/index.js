cdb9f21bea24a19a054f8cb9ac7f4bfbd2f4d0ac
Elise VanderWindt <elisevw62@gmail.com>
2020-12-20 11:17:14 -0500

    edits

--- BrowserBlog/pages/index.js
+++ BrowserBlog/pages/index.js
@@ -1,7 +1,7 @@
 export default [{"title":"About", "fname":"about.md"},
 {"title":"Work", "fname":"work.md"},
-//{"title":"Testing3", "fname":"portfolio.md"},
 {"title":"Contact", "fname":"contactFrom.md" },
 ];
 
 
+//{"title":"Testing3", "fname":"portfolio.md"},
